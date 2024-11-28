5394a74e61a1cb8ce010762530dcfa5027fc93f8
etabebe1 <etabebe1@gmail.com>
2024-11-27 20:36:56 -0500

    Modified Contrats

--- script/DeployVerseToken.s.sol
+++ script/DeployVerseToken.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.26;
 import {VerseToken} from "../src/VerseToken.sol";
 import {Script} from "forge-std/Script.sol";
 
-contract Token is Script {
+contract DeployVerseToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
     function run() external {
