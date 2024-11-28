cf51af0fd256458f0d39f61fc79373adf229fd31
etabebe1 <etabebe1@gmail.com>
2024-11-27 20:23:39 -0500

    Updated Makefile and README.md

--- script/DeployVerseToken.s.sol
+++ script/DeployVerseToken.s.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {Script} from "forge-std/";
+import {VerseToken} from "../src/VerseToken.sol";
+import {Script} from "forge-std/Script.sol";
 
-contract Token {}
+contract Token is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000 ether;
+
+    function run() external {
+        vm.startBroadcast();
+        new VerseToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+}
--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -2,12 +2,7 @@
 pragma solidity 0.8.19;
 
 interface tokenRecipient {
-    function receiveApproval(
-        address _from,
-        uint256 _value,
-        address _token,
-        bytes calldata _extraData
-    ) external;
+    function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external;
 }
 
 contract ManualToken {
@@ -26,11 +21,7 @@ contract ManualToken {
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     // This generates a public event on the blockchain that will notify clients
-    event Approval(
-        address indexed _owner,
-        address indexed _spender,
-        uint256 _value
-    );
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
@@ -40,11 +31,7 @@ contract ManualToken {
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
-    constructor(
-        uint256 initialSupply,
-        string memory tokenName,
-        string memory tokenSymbol
-    ) {
+    constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol) {
         totalSupply = initialSupply * 10 ** uint256(decimals); // Update total supply with the decimal amount
         balanceOf[msg.sender] = totalSupply; // Give the creator all initial tokens
         name = tokenName; // Set the name for display purposes
@@ -80,10 +67,7 @@ contract ManualToken {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transfer(
-        address _to,
-        uint256 _value
-    ) public returns (bool success) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         _transfer(msg.sender, _to, _value);
         return true;
     }
@@ -97,11 +81,7 @@ contract ManualToken {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _value
-    ) public returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= allowance[_from][msg.sender]); // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
@@ -116,10 +96,7 @@ contract ManualToken {
      * @param _spender The address authorized to spend
      * @param _value the max amount they can spend
      */
-    function approve(
-        address _spender,
-        uint256 _value
-    ) public returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
@@ -134,19 +111,10 @@ contract ManualToken {
      * @param _value the max amount they can spend
      * @param _extraData some extra information to send to the approved contract
      */
-    function approveAndCall(
-        address _spender,
-        uint256 _value,
-        bytes memory _extraData
-    ) public returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData) public returns (bool success) {
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
-            spender.receiveApproval(
-                msg.sender,
-                _value,
-                address(this),
-                _extraData
-            );
+            spender.receiveApproval(msg.sender, _value, address(this), _extraData);
             return true;
         }
     }
@@ -174,10 +142,7 @@ contract ManualToken {
      * @param _from the address of the sender
      * @param _value the amount of money to burn
      */
-    function burnFrom(
-        address _from,
-        uint256 _value
-    ) public returns (bool success) {
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value); // Check if the targeted balance is enough
         require(_value <= allowance[_from][msg.sender]); // Check allowance
         balanceOf[_from] -= _value; // Subtract from the targeted balance
