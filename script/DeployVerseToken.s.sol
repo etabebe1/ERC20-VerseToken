fa9a88f4aebf539b2f50710667c6db54159da5dc
etabebe1 <etabebe1@gmail.com>
2024-11-28 01:16:01 -0500

    Contract updated

--- script/DeployVerseToken.s.sol
+++ script/DeployVerseToken.s.sol
@@ -7,9 +7,11 @@ import {Script} from "forge-std/Script.sol";
 contract DeployVerseToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
-    function run() external {
+    function run() external returns (VerseToken) {
         vm.startBroadcast();
-        new VerseToken(INITIAL_SUPPLY);
+        VerseToken verseToken = new VerseToken(INITIAL_SUPPLY);
         vm.stopBroadcast();
+
+        return verseToken;
     }
 }
--- test/VerseTokenTest.t.sol
+++ test/VerseTokenTest.t.sol
@@ -1,4 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract VerseTokenTest {}
+import {Test} from "forge-std/Test.sol";
+import {VerseToken} from "../src/VerseToken.sol";
+import {DeployVerseToken} from "../script/DeployVerseToken.s.sol";
+
+contract VerseTokenTest is Test {
+    VerseToken public verseToken;
+    DeployVerseToken public deployVerseToken;
+
+    address Jeremiah = makeAddr("jeremiah");
+    address Alice = makeAddr("Alice");
+
+    uint256 public constant INITIAL_BALANCE = 10 ether;
+
+    function setUp() public {
+        DeployVerseToken deployer = new DeployVerseToken();
+        verseToken = deployer.run();
+
+        vm.prank(msg.sender);
+        verseToken.transfer(Jeremiah, INITIAL_BALANCE);
+    }
+
+    function testJeremiahBalance() public {
+        vm.prank(Jeremiah);
+        vm.deal(Jeremiah, INITIAL_BALANCE);
+
+        assertEq(verseToken.balanceOf(Jeremiah), INITIAL_BALANCE);
+    }
+
+    function testInitialSupply() public view {}
+
+    function testAllowances() public {
+        uint256 initialAllowance = 1000;
+
+        // Bob approves Alice to spend tokens on his behalf
+
+        vm.prank(Jeremiah);
+        verseToken.approve(Alice, initialAllowance);
+        uint256 transferAmount = 500;
+
+        vm.prank(Alice);
+        verseToken.transferFrom(Jeremiah, Alice, transferAmount);
+        assertEq(verseToken.balanceOf(Alice), transferAmount);
+        assertEq(
+            verseToken.balanceOf(Jeremiah),
+            INITIAL_BALANCE - transferAmount
+        );
+    }
+}
